ae885416f5f19df8e277b2e11ab46a054f4679e7
junwvwv <wvwv0612@gmail.com>
2023-10-15 18:13:59 +0900

    외부 설정 - 스프링 통합

---
+++ external/src/main/java/hello/EnvironmentCheck.java
@@ -0,0 +1,31 @@
+package hello;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EnvironmentCheck {
+
+    private final Environment env; //특정 외부 설정에 종속되지 않고 일관성 있게 key=value 형식의 외부 설정에 접근 가능
+
+    public EnvironmentCheck(Environment env) {
+        this.env = env;
+    }
+
+    @PostConstruct
+    public void init() {
+        String url = env.getProperty("url");
+        String user = env.getProperty("user");
+        String password = env.getProperty("password");
+        log.info("env url = {}", url);
+        log.info("env user = {}", user);
+        log.info("env password = {}", password);
+
+        //우선순위 커맨드 라인 옵션 인수 > 자바 시스템 속성
+        //더 유연하고 범위가 좁은 것이 우선권을 가진다
+    }
+
+}
